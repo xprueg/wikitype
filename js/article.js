410a343d4e796fd1556159e120eee7af1f73ec9d
xprueg <github@xpr.org>
2021-12-03 22:05:17 +0100

    Display unaltered wikipedia content and handle untypeable chars in the background

--- js/article.js
+++ js/article.js
@@ -1,3 +1,162 @@
+class InputData {
+    constructor(string) {
+        this.data = string;
+        this.idx = 0;
+    }
+
+    static new (...args) {
+        return new InputData(...args);
+    }
+
+    get char() {
+        return this.data[this.idx];
+    }
+
+    set char(c) {
+        const [before, after] = [this.data.substr(0, this.idx), this.data.substr(this.idx + 1)];
+        this.data = before + c + after;
+    }
+
+    get len() {
+        return this.data.length;
+    }
+
+    peek(chars = 1) {
+        return this.data.substr(this.idx + 1, chars);
+    }
+
+    eat(chars = 1) {
+        this.idx += chars;
+    }
+}
+
+class InputState {
+    constructor(token, input) {
+        this.token = InputData.new(token);
+        this.input = InputData.new(input);
+
+        this.correct = String();
+        this.upcoming = String();
+        this.mistyped = String();
+    }
+
+    static new(...args) {
+        return new InputState(...args);
+    }
+
+    compare() {
+        for (;this.token.idx < this.token.len;) {
+            if (!this.input.char) {
+                this.upcoming = this.token.data.substr(this.token.idx);
+                break;
+            }
+
+            if (this.token.char !== this.input.char) {
+                const corrected = this.autocorrect();
+                if (!(corrected ^ InputState.NOT_CORRECTED)) {
+                    this.upcoming = this.token.data.substr(this.token.idx);
+                    this.mistyped = this.input.data.substr(this.input.idx);
+                    return false;
+                } else if (!(corrected ^ InputState.RETRY)) {
+                    continue;
+                }
+            }
+
+            ++this.token.idx;
+            ++this.input.idx;
+        }
+
+        this.correct = this.token.data.substr(0, this.token.idx);
+
+        return this.correct === this.token.data;
+    }
+
+    autocorrect(state) {
+        const token_c = this.token.char;
+        const input_c = this.input.char;
+
+        for (;;) {
+            // Combining Acute Accent
+            if ("\u0301" === token_c) {
+                this.token.eat();
+                return InputState.RETRY;
+            }
+
+            // Hyphens
+            // U+2010 Hyphen
+            // U+2011 Non-Breaking Hyphen
+            // U+2012 Figure Dash
+            // U+2013 En Dash
+            // U+2014 Em Dash
+            // U+2015 Horizontal Bar
+            // U+2212 Minus Sign
+            if (/[\u2010-\u2015\u2212]/.test(token_c) && /[-–—]/.test(input_c))
+                break;
+
+            // Single Quotation Marks
+            // U+2018 Left Single Quotation Mark
+            // U+2019 Right Single Quotation Mark
+            // U+201A Single Low-9 Quotation Mark
+            // U+201B Single High-Reversed-9 Quotation Mark
+            // U+2032 Prime
+            if (/[\u2018-\u201B\u2032]/.test(token_c) && /["'‘]/.test(input_c))
+                break;
+
+            // Double Quotation Marks
+            // U+201C Left Double Quotation Mark
+            // U+201D Right Double Quotation Mark
+            // U+201E Double Low-9 Quotation Mar
+            // U+201F Double High-Reversed-9 Quotation Mark
+            // U+2033 Double Prime
+            if (/[\u201C-\u201F\u2033]/.test(token_c) && /["']/.test(input_c))
+                break;
+
+            // Sharp S
+            // U+00DF Latin Small Letter Sharp S
+            // U+1E9E Latin Capital Letter Sharp S
+            if (/[\u00DF\u1E9E]/.test(token_c) && /^(ss|ß)$/.test(input_c + this.input.peek())) {
+                this.input.eat();
+                break;
+            }
+
+            // Whitespace & Zero Width Space
+            // U+200B Zero Width Space (ZWSP)
+            if (/\s|\u200B/.test(token_c) && /\s/.test(input_c))
+                break;
+
+
+            // Fractions
+            // U+2044 Fraction Slash
+            if (/[\u2044]/.test(token_c) && '/' === input_c)
+                break;
+
+            if (/\d+\u2044\d*/.test(token_c.normalize("NFKD"))) {
+                const fraction = token_c.normalize("NFKD").replace("\u2044", "/");
+                const user_input = input_c + this.input.peek(fraction.length - 1);
+
+                if (fraction === user_input) {
+                    this.input.eat(fraction.length - 1);
+                    break;
+                }
+            }
+
+            // Normalization
+            // Superscripts and Subscripts
+            if (token_c.normalize("NFKD") === input_c)
+                if (token_c.normalize("NFKD").length === input_c.length)
+                    break;
+
+            return InputState.NOT_CORRECTED;
+        }
+
+        return InputState.CORRECTED;
+    }
+}
+
+InputState.CORRECTED = 1 << 0;
+InputState.NOT_CORRECTED = 1 << 1;
+InputState.RETRY = 1 << 2;
+
 class ArticleData {
     constructor(data) {
         this._raw = data;
@@ -21,23 +180,7 @@ class ArticleData {
     /// [>] text: String
     /// [<] Array<String*>
     get tokenized_extract() {
-        return this.extract
-                   // Remove Trailing Whitespace
-                   .replace(/(\s*|\n)$/, String())
-                   // Replace Whitespace
-                   .replace(/\s+/g, "\x20")
-                   // Replace Fraction Slash ( ⁄ )
-                   .replace(/[\u2044]/, "\x2F")
-                   // Replace Hyphens ( ‐ ‑ ‒ – — ― − )
-                   .replace(/[\u2010-\u2015\u2212]/g, "\x2D")
-                   // Replace Single Quotation Marks ( ‘ ’ ‚ ‛ )
-                   .replace(/[\u2018-\u201B]/g, "\x27")
-                   // Replace Double Quotation Marks ( “ ” „ ‟ )
-                   .replace(/[\u201C-\u201F]/g, "\x22")
-                   // Remove Combining Acute Accent ( ◌́ )
-                   .replace(/\u0301/, String())
-                   // Split text into words
-                   .match(/[^\s]+\s{0,1}/g);
+        return this.extract.trim().match(/[^\s]+\s{0,1}/g);
     }
 }
 
@@ -263,6 +406,7 @@ void new class Article extends Controller {
 
     /// Focuses the input element if it's not the active element.
     ///
+    /// [~] TODO: Make sure that the input gets properly focused on the focus event.
     /// [<] void
     focus_input() {
         if (document.activeElement !== this.$input) {
@@ -286,7 +430,7 @@ void new class Article extends Controller {
     }
 
     input_evt(e) {
-        const input_txt = this.$input.textContent.replace(/\u00A0/g, "\x20");
+        const input_txt = this.$input.textContent;
         const token_txt = this.$active_token.dataset.word;
 
         // Don't update on composing keys as it will show up as mistyped text.
@@ -298,28 +442,23 @@ void new class Article extends Controller {
         if (!token_txt)
             return this.clear_input();
 
-        let upcoming_txt = String();
-        let mistyped_txt = String();
-
-        if (token_txt.indexOf(input_txt) === 0) {
-            if (token_txt.length === input_txt.length) {
-                this.advance_token();
-                this.clear_input();
-                return;
-            }
-
-            upcoming_txt = token_txt.substr(input_txt.length);
-        } else {
-            for (let i = 0; i < input_txt.length; ++i) {
-                if (input_txt[i] !== token_txt[i]) {
-                    upcoming_txt += token_txt.substr(i);
-                    mistyped_txt = input_txt.substr(i);
-                    break;
-                }
-            }
+        const state = InputState.new(token_txt, input_txt);
+        if (state.compare()) {
+            this.advance_token();
+            this.clear_input();
+            return;
         }
 
-        this.$input.dataset.mistyped = mistyped_txt;
-        this.$active_token.dataset.upcoming = upcoming_txt;
+        if (state.correct) {
+            this.$input.textContent = state.correct + state.mistyped;
+            const range = document.createRange();
+            const selection = window.getSelection();
+            range.selectNodeContents(this.$input);
+            range.collapse(false);
+            selection.empty();
+            selection.addRange(range);
+        }
+        this.$input.dataset.mistyped = state.mistyped;
+        this.$active_token.dataset.upcoming = state.upcoming;
     }
 }
