305a709cdb5a5aa02e9816df77a0b7d2e14bd3bf
xprueg <github@xpr.org>
2021-12-24 19:58:50 +0100

    'Pro' theme changes

--- js/settings/themes/pro.js
+++ js/settings/themes/pro.js
@@ -3,12 +3,39 @@ const PRO_THEME = {
     id     : "pro",
     extend : "base",
 
+    // Custom
+    __cSize              : "2px",
+    __cBgSize            : "calc(__cSize * 2)",
+    __cColor             : "__dark",
+    __cHalfBgSize        : "calc(__cBgSize / 2)",
+    __cHeight            : "calc(__mainPadding * .5)",
+    __cCheckerBackground : `
+        // Overlay
+        linear-gradient(__bright, __bright) 0 0/100% calc(100% - __cHeight) no-repeat,
+
+        // Checker Pattern
+        linear-gradient(45deg, __cColor, __cColor 25%, transparent 25%)
+                        0 0/__cBgSize __cBgSize,
+        linear-gradient(-45deg, __cColor, __cColor 25%, transparent 25%)
+                        0 calc(-1 * __cHalfBgSize)/__cBgSize __cBgSize,
+        linear-gradient(45deg, transparent, transparent 75%, __cColor 75%)
+                        calc(-1 * __cHalfBgSize) __cHalfBgSize/__cBgSize __cBgSize,
+        linear-gradient(-45deg, transparent, transparent 75%, __cColor 75%)
+                        __cHalfBgSize 0/__cBgSize __cBgSize,
+
+        // Background
+        linear-gradient(__bright, __bright)
+    `,
+
     // General
-    __dark   : "#1a1c2c",
-    __bright : "#f4f4f4",
+    __dark           : "hsl(.90, 90%, .43%)",
+    __bright         : "hsl(43.90, 43%, 90%)",
+    __bodyBackground : "__cCheckerBackground",
 
     // Settings
-    __settingsBoxShadow : "0 0 1000px 100px __dark",
+    __settingsBackground   : "__cCheckerBackground",
+    __settingsHeadingColor : "__bright",
+    __settingsBoxShadow    : "0 0 1000px 100px __dark",
 
     // Article
     __articleBaseWidth           : "900px",
