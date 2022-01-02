09875ef750543e411747ef228e0c1085004c2a12
xprueg <github@xpr.org>
2022-01-02 23:11:44 +0100

    History: Use class for history entries

--- js/history.js
+++ js/history.js
@@ -1,3 +1,26 @@
+class HistoryEntry {
+    constructor({ wikiapi_response, is_related }) {
+        this.wikiapi_response = wikiapi_response;
+        this.is_related = is_related;
+
+        const $li = ª(ƒ("#historyEntryTemplate"), "li");
+        const $span = $li.querySelector("span");
+
+        if (!is_related)
+            $li.dataset.chain = "start";
+
+        $li.dataset.id = wikiapi_response.pageid;
+        $span.innerText = wikiapi_response.titles.normalized;
+        $span.dataset.lang = wikiapi_response.lang;
+        this.$node = $li;
+        this.$text = $span;
+    }
+
+    get id() {
+        return String(this.wikiapi_response.pageid);
+    }
+}
+
 void new class History extends Controller {
     __data() {
         this.$node = ƒ(".history");
@@ -32,36 +55,32 @@ void new class History extends Controller {
     /// [>] $entry: HTMLLIELEMENT
     /// [<] VOID
     restore_entry($entry) {
-        while($entry.previousElementSibling) {
-            this.cache.delete($entry.previousElementSibling.dataset.pageid);
-            $entry.previousElementSibling.remove();
+        while ($entry.previousElementSibling) {
+            const id = $entry.previousElementSibling.dataset.id;
+            const history_entry = this.cache.get(id);
+
+            history_entry.$node.remove();
+            this.cache.delete(id);
         }
 
-        emit`article :setContents`(this.cache.get($entry.dataset.pageid));
+        const entry_wikiapi_response = this.cache.get($entry.dataset.id).wikiapi_response;
+        emit`article :setContents`(entry_wikiapi_response);
         emit`nav :forceHide`();
         emit`input :clear`();
     }
 
     add_wpm_to_page_id(wpm, pageid) {
-        const $history_item = ƒ(`[data-pageid="${pageid}"]`);
-        if (!$history_item)
+        const history_entry = this.cache.get(String(pageid));
+        if (!history_entry)
             return;
 
-        ƒ("[data-lang]", $history_item).dataset.wpm = wpm;
+        history_entry.$text.dataset.wpm = wpm;
     }
 
     push_entry({ wikiapi_response, is_related }) {
-        const $li = ª(ƒ("#historyEntryTemplate"), "li");
-        const $span = $li.querySelector("span");
+        const entry = new HistoryEntry({ wikiapi_response, is_related });
 
-        if (!is_related)
-            $li.dataset.chain = "start";
-
-        $li.dataset.pageid = wikiapi_response.pageid;
-        $span.innerText = wikiapi_response.titles.normalized;
-        $span.dataset.lang = wikiapi_response.lang;
-        this.$node.insertBefore($li, this.$node.children.length ? this.$node.children[0] : null);
-
-        this.cache.set(String(wikiapi_response.pageid), wikiapi_response);
+        this.$node.insertBefore(entry.$node, this.$node.children.length ? this.$node.children[0] : null);
+        this.cache.set(entry.id, entry);
     }
 }
