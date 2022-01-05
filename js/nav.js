d98a641830f38aa9069b4a3a806dc84a611ac03b
xprueg <github@xpr.org>
2022-01-05 21:43:40 +0100

    Nav: Use new controller class

--- js/nav.js
+++ js/nav.js
@@ -1,53 +1,55 @@
-void function NavController() {
-    const self = Object.create(null);
+void new class Nav extends Controller {
+    __data() {
+        this.$node = ƒ("nav");
+        this.related_nodes = ƒƒ(".navOption", this.$node).slice(1);
+        this.related_article_buffer;
+    }
 
-    void function init() {
-        self.node = ƒ("nav");
-        self.related_nodes = ƒƒ(".navOption", self.node).slice(1);
-        self.related_article_buffer;
+    __listen() {
+        return {
+            "article :afterUnload": article_data => (this.render_options(article_data), this.show()),
+            "nav :displayOptions": article_data => (this.render_options(article_data), this.show()),
+            "nav :forceHide": () => this.hide(),
+        };
+    }
 
-        hide();
+    __init() {
+        this.hide();
 
         document.body.addEventListener("keydown", e => {
-            if (is_hidden())
+            if (this.is_hidden())
                 return;
 
             e.preventDefault();
             e.stopPropagation();
 
-            select(e.key);
-            hide();
+            this.select(e.key);
+            this.hide();
         }, true);
-
-        listen({
-            "article :afterUnload": article_data => (render_options(article_data), show()),
-            "nav :displayOptions": article_data => (render_options(article_data), show()),
-            "nav :forceHide": () => hide(),
-        });
-    }();
-
-    function show() {
-        self.node.dataset.isVisible = true;
     }
 
-    function hide() {
-        self.node.dataset.isVisible = false;
+    show() {
+        this.$node.dataset.isVisible = true;
     }
 
-    function is_hidden() {
-        return self.node.dataset.isVisible === String("false");
+    hide() {
+        this.$node.dataset.isVisible = false;
     }
 
-    function reset_options() {
-        self.related_article_buffer = Array();
-        self.related_nodes.forEach(node => {
+    is_hidden() {
+        return this.$node.dataset.isVisible === String("false");
+    }
+
+    reset_options() {
+        this.related_article_buffer = Array();
+        this.related_nodes.forEach(node => {
             node.dataset.status = "loading";
             ƒ(".navOptionTitle", node).textContent = String();
         });
     }
 
-    function select(choice) {
-        const wikiapi_response = self.related_article_buffer[Number(choice) - 1];
+    select(choice) {
+        const wikiapi_response = this.related_article_buffer[Number(choice) - 1];
         if (wikiapi_response) {
             emit`history :push`({ wikiapi_response, is_related: true });
             emit`article :setContents`(wikiapi_response);
@@ -60,21 +62,21 @@ void function NavController() {
         }
     }
 
-    function render_options(article_data) {
-        reset_options();
+    render_options(article_data) {
+        this.reset_options();
 
         req`wikiapi :fetchRelatedArticles`(article_data._raw).then(articles => {
             const related_articles = articles.pages.filter(
                 article => !req`history :includesPageId`(article.pageid)
             );
 
-            self.related_nodes.forEach(node => {
+            this.related_nodes.forEach(node => {
                 const related_article_data = related_articles
                     .splice(Math.floor(Math.random() * related_articles.length), 1)
                     .pop();
 
                 if (related_article_data) {
-                    self.related_article_buffer.push(related_article_data);
+                    this.related_article_buffer.push(related_article_data);
                     node.dataset.status = "available";
                     ƒ(".navOptionTitle", node).textContent = related_article_data.titles.normalized;
                 } else {
